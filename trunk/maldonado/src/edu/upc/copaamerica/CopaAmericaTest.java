e6c8dd59ade2f8aac439e13712f53e59bb0696d9
x.nior.x@gmail.com <x.nior.x@gmail.com@58a0db45-8e36-0fa3-17bd-f9d99f901bf3>
2011-07-09 15:02:16 +0000

    primera carga

---
+++ trunk/maldonado/src/edu/upc/copaamerica/CopaAmericaTest.java
@@ -0,0 +1,24 @@
+package edu.upc.copaamerica;
+
+public class CopaAmericaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*Equipo equipo1 = new Equipo();
+		equipo1.setNombre("Seleccion peruana");
+		equipo1.setPais("Peru");
+
+		Equipo equipo2 = new Equipo();
+		equipo2.setNombre("Seleccion Uruguaya");
+		equipo2.setPais("Uruguay");
+		Equipo equipo3 = new Equipo("Seleccion Argentina","Argentina");
+		System.out.println("El equipo 1 es de " + equipo1.getPais());
+		System.out.println("El equipo 2 es de " + equipo2.getPais());
+		*/
+		
+		Equipo eq1= Equipo.crearEquipo();
+	}
+
+}
